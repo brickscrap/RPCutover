bf6feec50c4252dfb3d82c144a761520675e88fd
Gary MacGregor-Manzi <omgitsgary90@gmail.com>
2021-12-09 16:55:49 +0000

    Remove Get/Upload Commands
    Too much effort for too little return

--- RPCutover/AppService.cs
+++ RPCutover/AppService.cs
@@ -16,17 +16,6 @@ namespace RPCutover
 
         public async Task<int> Run(string[] args)
         {
-            var getCommand = new Command("get", "Collect the files from the RP sFTP server for both environvments (once they have been exported).")
-            {
-                new Argument<string>("internal-buid", "The Red Prairie internal business unit ID. Used to find and pull the stock export files from the sFTP server."),
-                new Argument<string>("station-name", "Name of the station. Used to generate a folder to save the files to."),
-            };
-
-            getCommand.Handler = CommandHandler.Create<string, string>(async (buid, stationName) => 
-            { 
-                await GetCommandHandler.Handle(_config, buid, stationName);
-            });
-
             var convertCommand = new Command("convert", "Shift the stock data from the DN file into the TK file, and generate a file ready for upload back to RP.")
             {
                 new Argument<string>("folder-path", "Path to the site folder previously created using the get command.")
@@ -37,14 +26,8 @@ namespace RPCutover
                 await ConvertCommandHandler.Handle(_config, folderPath);
             });
 
-            var uploadCommand = new Command("upload", "Upload the newly created stock file back to the RedPrairie sFTP server.")
-            {
-                new Argument<string>("file-path", "Path to the file created during the 'convert' command.")
-            };
-
             var rootCommand = new RootCommand
             {
-                getCommand,
                 convertCommand
             };
 
